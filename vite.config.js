76e2f0bba143f9443230327505abb1fdaa80bdcb
Soufianerfq <soufianerafiq1999@gmail.com>
2025-03-31 22:45:51 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "appointment-scheduler",
+  base: "/appointment-scheduler",
   plugins: [react(), tailwindcss()],
 })
