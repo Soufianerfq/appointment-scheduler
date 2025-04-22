4233016cb6895cb57287d901f5c96160530bf7c0
soufianerfq <soufianerafiq1999@gmail.com>
2025-04-22 11:45:38 +0100

    api link update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5500",
+  baseURL: "https://appointment-api-production-5b4f.up.railway.app/",
 });
 
 export const axiosPrivate =  axios.create({
-  baseURL: "http://localhost:5500",
+  baseURL: "https://appointment-api-production-5b4f.up.railway.app/",
   headers: {"Content-Type": "application/json"},
   withCredentials: true
 });
